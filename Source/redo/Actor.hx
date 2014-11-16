dc0f36777a1ec92255a7e1e7556f3de3bbaf9454
Thomas BAUDON <baudon.thomas@gmail.com>
2014-11-16 01:59:03 +0100

    collision glitchy

--- Source/redo/Actor.hx
+++ Source/redo/Actor.hx
@@ -12,6 +12,8 @@ class Actor extends Entity
 	
 	var mLevel : Level;
 	var mNextPos : Vec2;
+	
+	var mOnFloor : Bool;
 
 	public function new(level : Level) 
 	{
@@ -33,28 +35,73 @@ class Actor extends Entity
 	{
 		super.update(delta);
 		
+		mOnFloor = false;
+		
 		vel.x += mLevel.getGravity().x;
 		vel.y += mLevel.getGravity().y;
 		
 		mNextPos.x = pos.x + vel.x * delta;
 		mNextPos.y = pos.y + vel.y * delta;
 		
-		var tileToTestOnX = Math.ceil(dim.x / mLevel.getTileWidth());
-		if (vel.y > 0) {
-			var tileAtBottomLeft = mLevel.getTileAt(mNextPos.x, mNextPos.y + dim.y);
-			var tileAtBottomRight = mLevel.getTileAt(mNextPos.x + dim.x, mNextPos.y + dim.y);
-			
-			if (tileAtBottomLeft != 0 || tileAtBottomRight != 0) {
-				mNextPos.y = Std.int((mNextPos.y + dim.y) / mLevel.getTileHeight()) * mLevel.getTileHeight() - dim.y;
-				vel.y = 0;
-			}
-		}else if (vel.y < 0) {
-			
-		}
+		resolveCollisionWithMap();
 		
 		
 		
 		
 		pos.copy(mNextPos);
+	}
+	
+	function resolveCollisionWithMap():Void 
+	{
+		if(vel.x > vel.y){
+			resolveXCollision();
+			resolveYCollision();
+		}else {
+			resolveYCollision();
+			resolveXCollision();
+		}
+	}
+	
+	function resolveYCollision():Void 
+	{
+		if (vel.y > 0) {
+			var tileAtBottomLeft = mLevel.getTileAt(mNextPos.x + 1, mNextPos.y + dim.y );
+			var tileAtBottomRight = mLevel.getTileAt(mNextPos.x + dim.x - 1, mNextPos.y + dim.y);
+			
+			if (tileAtBottomLeft != 0 || tileAtBottomRight != 0) {
+				mNextPos.y = Std.int((mNextPos.y + dim.y) / mLevel.getTileHeight()) * mLevel.getTileHeight() - dim.y;
+				vel.y = 0;
+				mOnFloor = true;
+			}
+		}else if (vel.y < 0) {
+			var tileAtTopLeft = mLevel.getTileAt(mNextPos.x + 1, mNextPos.y);
+			var tileAtTopRight = mLevel.getTileAt(mNextPos.x + dim.x - 1, mNextPos.y);
+			
+			if (tileAtTopLeft != 0 || tileAtTopRight != 0) {
+				mNextPos.y = Std.int(mNextPos.y / mLevel.getTileHeight()) * mLevel.getTileHeight() + mLevel.getTileHeight();
+				vel.y = 0;
+			}
+		}
+	}
+	
+	function resolveXCollision():Void 
+	{
+		if (vel.x > 0) {
+			var tileAtTopRight = mLevel.getTileAt(mNextPos.x + dim.x , mNextPos.y + 1);
+			var tileAtBottomRight = mLevel.getTileAt(mNextPos.x + dim.x , mNextPos.y + dim.y - 1);
+			
+			if (tileAtTopRight != 0 || tileAtBottomRight != 0) {
+				mNextPos.x = Std.int((mNextPos.x + dim.x) / mLevel.getTileWidth()) * mLevel.getTileWidth() - dim.x;
+				vel.x = 0;
+			}
+		}else if (vel.x < 0) {
+			var tileAtTopLeft = mLevel.getTileAt(mNextPos.x , mNextPos.y + 1);
+			var tileAtBottomLeft = mLevel.getTileAt(mNextPos.x , mNextPos.y + dim.y - 1);
+			
+			if (tileAtTopLeft != 0 || tileAtBottomLeft != 0) {
+				mNextPos.x = Std.int(mNextPos.x / mLevel.getTileWidth()) * mLevel.getTileWidth() + mLevel.getTileWidth();
+				vel.x = 0;
+			}
+		}
 	}
 }
--- Source/redo/Hero.hx
+++ Source/redo/Hero.hx
@@ -1,7 +1,10 @@
 package redo;
 import geom.Vec2;
 import openfl.display.BitmapData;
+import openfl.events.KeyboardEvent;
 import openfl.geom.Rectangle;
+import openfl.Lib;
+import openfl.ui.Keyboard;
 
 /**
  * ...
@@ -9,12 +12,50 @@ import openfl.geom.Rectangle;
  */
 class Hero extends Actor
 {
+	
+	var mJumpDown : Bool;
+	var mXAxis : Float;
 
 	public function new(level : Level) 
 	{
 		super(level);
+		mXAxis = 0;
+		Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+		Lib.current.stage.addEventListener(KeyboardEvent.KEY_UP, onKeyUp);
 	}
 	
+	private function onKeyUp(e:KeyboardEvent):Void 
+	{
+		switch (e.keyCode) {
+			case Keyboard.SPACE :
+				mJumpDown = false;
+			case Keyboard.Q :
+				mXAxis = 0;
+			case Keyboard.D :
+				mXAxis = 0;
+		}
+	}
 	
+	private function onKeyDown(e:KeyboardEvent):Void 
+	{
+		switch (e.keyCode) {
+			case Keyboard.SPACE :
+				mJumpDown = true;
+			case Keyboard.Q :
+				mXAxis = -1;
+			case Keyboard.D :
+				mXAxis = 1;
+		}
+	}
+	
+	override function update(delta:Float) 
+	{
+		super.update(delta);
+		
+		if (mOnFloor && mJumpDown)
+			vel.y -= 340;
+			
+		vel.x += mXAxis * delta * 1000;
+	}
 	
 }
